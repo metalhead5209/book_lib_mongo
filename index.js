2780cbb8412af711fc652e7982db91dbfc10a325
Aaron Rodi <metalhead5209@gmail.com>
2022-07-02 09:17:29 -0500

    added handlebars and got index to render through route

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const handlebars = require('express-handlebars');
+const methodOverride = require('method-override');
+const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
+const Book = require('./server/model/book');
 
 const app = express();
 const PORT = 5050 || process.env.PORT;
 
 const dbURL = process.env.DB_URL || "mongodb://127.0.0.1:27017/bookLibrary";
 
+// DB CONNECTION
 mongoose.connect(
     dbURL,
     () => {
@@ -14,6 +20,26 @@ mongoose.connect(
     },
     (e) => console.error('NO CONNECTION', e)
 );
+
+
+// TEMPLATE ENGINE
+app.engine("hbs", handlebars.engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
+app.use(methodOverride('_method'));
+app.use(bodyParser.json());
+
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`On port ${PORT}`)
