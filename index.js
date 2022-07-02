aaaf0bc4df7256ccbd3d7dfd4e127ef2c52bb3e5
Aaron Rodi <metalhead5209@gmail.com>
2022-07-02 12:46:07 -0500

    finished new and add book routes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const handlebars = require('express-handlebars');
-const methodOverride = require('method-override');
-const dotenv = require('dotenv').config();
-const bodyParser = require('body-parser');
-const Book = require('./server/model/book');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const handlebars = require("express-handlebars");
+const methodOverride = require("method-override");
+const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
+const Book = require("./server/model/book");
 
 const app = express();
 const PORT = 5050 || process.env.PORT;
@@ -14,13 +14,12 @@ const dbURL = process.env.DB_URL || "mongodb://127.0.0.1:27017/bookLibrary";
 
 // DB CONNECTION
 mongoose.connect(
-    dbURL,
-    () => {
-        console.log('Connected to DB');
-    },
-    (e) => console.error('NO CONNECTION', e)
+  dbURL,
+  () => {
+    console.log("Connected to DB");
+  },
+  (e) => console.error("NO CONNECTION", e)
 );
-
 
 // TEMPLATE ENGINE
 app.engine("hbs", handlebars.engine({ extname: ".hbs" }));
@@ -28,35 +27,30 @@ app.set("view engine", "hbs");
 
 // MIDDLEWARE
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static("public"));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 app.use(bodyParser.json());
-
 
 //            *** ROUTES ***
 
 // Book Library (index)
-app.get('/', async (req, res) => {
-    const book = await Book.find({}).lean()
-    res.render('index', { book });
+app.get("/", async (req, res) => {
+  const book = await Book.find({}).lean();
+  res.render("index", { book });
 });
 
 // New Book Form
-app.get('/new', (req, res) => {
-    res.render('new');
-})
+app.get("/new", (req, res) => {
+  res.render("new");
+});
 
-app.post('/', async (req, res) => {
-
-})
-
-
-
-
-
-
+app.post("/", async (req, res) => {
+    const book = new Book(req.body);
+    await book.save();
+    res.redirect('/');
+});
 
 app.listen(PORT, () => {
-    console.log(`On port ${PORT}`)
+  console.log(`On port ${PORT}`);
 });
